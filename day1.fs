bbf5289101213b28e5fad1906b7f4de5eb575226
Kieran Molloy <kieran.molloy@pushpay.com>
2022-12-02 16:35:19 +1300

    Day 1 - Challenges

---
+++ day1.fs
@@ -0,0 +1,34 @@
+﻿module adventofcode
+
+open Xunit
+open FsUnit.Xunit
+open System.IO
+
+let challenge1Data () =
+    seq {
+        let numbers = File.ReadLines("../../../advent1.txt")
+        let mutable x = []
+
+        for number in numbers do
+            if number = "" then yield x
+            x <- if number = "" then [] else (int number) :: x
+
+        yield x
+    }
+
+
+[<Fact>]
+let Challenge1 () =
+    challenge1Data ()
+    |> Seq.map List.sum
+    |> Seq.max
+    |> should equal 69626
+
+[<Fact>]
+let Challenge1A () =
+    challenge1Data ()
+    |> Seq.map List.sum
+    |> Seq.sortDescending
+    |> Seq.take 3
+    |> Seq.sum
+    |> should equal 206780
