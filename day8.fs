fb165c5783662b0c4b873f447067894f00ba0be8
Kieran Molloy <kieran.molloy@pushpay.com>
2022-12-09 18:03:52 +1300

    Day 8 - Challenge part 2

--- day8.fs
+++ day8.fs
@@ -85,3 +85,58 @@ let Challenge8 () =
     File.ReadLines "day8.txt"
     |> countAllVisibleTrees
     |> should equal 1713
+
+type Direction = Up | Down | Left | Right
+
+let rec countTreesVisibleAtHeightFromPositionInDirection (grid: int[,]) (y: int) (x: int) (height: int) (direction: Direction) =
+    match direction with
+    | Up when y = 0 -> 0
+    | Up when grid[y - 1, x] >= height -> 1
+    | Up -> 1 + countTreesVisibleAtHeightFromPositionInDirection grid (y - 1) x height Up
+    | Down when y = Array2D.length2 grid - 1 -> 0
+    | Down when grid[y + 1, x] >= height -> 1
+    | Down -> 1 + countTreesVisibleAtHeightFromPositionInDirection grid (y + 1) x height Down
+    | Left when x = 0 -> 0
+    | Left when grid[y, x - 1] >= height -> 1
+    | Left -> 1 + countTreesVisibleAtHeightFromPositionInDirection grid y (x - 1) height Left
+    | Right when x = Array2D.length1 grid - 1 -> 0
+    | Right when grid[y, x + 1] >= height -> 1
+    | Right -> 1 + countTreesVisibleAtHeightFromPositionInDirection grid y (x + 1) height Right
+
+let calculateScenicScore (grid: int[,]) (y: int) (x: int) (height: int) =
+    [| Up; Down; Left; Right |]
+    |> Seq.map (countTreesVisibleAtHeightFromPositionInDirection grid y x height)
+    |> Seq.reduce (*)
+
+let calculateAllScenicScores (input: string seq) =
+    let grid = parseGrid input
+    grid |> Array2D.mapi (calculateScenicScore grid)
+
+[<Fact>]
+let Challenge8ASamplePart1 () =
+    let grid = parseGrid sampleInput
+
+    calculateScenicScore grid 1 2 grid[1,2]
+    |> should equal 4
+
+    calculateScenicScore grid 3 2 grid[3,2]
+    |> should equal 8
+
+[<Fact>]
+let Challenge8ASample () =
+    let scores = calculateAllScenicScores sampleInput
+    scores[1,2] |> should equal 4
+    scores[3,2] |> should equal 8
+    
+    scores
+    |> flatten
+    |> Seq.max
+    |> should equal 8
+
+[<Fact>]
+let Challenge8A () =
+    File.ReadLines "day8.txt"
+    |> calculateAllScenicScores
+    |> flatten
+    |> Seq.max
+    |> should equal 268464
